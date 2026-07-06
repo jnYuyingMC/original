d5cad4fcba49e7b183c1364e8f87da01c292efb1
Yuehang Ran <70203506+Kotobaxiao-David@users.noreply.github.com>
2026-07-06 15:53:50 +0800

    Disable deprecated self-hosted VR source button

--- vr.js
+++ vr.js
@@ -1,14 +1,13 @@
 function choose() {
     mdui.dialog({
         title: '请选择源',
-        content: '<p>自建源：使用了Google提供的框架，速度更快但可能不稳定</p><p>Gayhub：使用了Google提供的框架，国内访问加载速度极慢</p><p>YouTube：在某些国家或地区不存在</p><p>720yun：站在访问者的角度这个应该是最好的，但因为他是第三方商业所以我们并不推荐</p>',
+        content: '<p>自建源(Deprecated)：使用了Google提供的框架，速度更快但可能不稳定</p><p>Gayhub：使用了Google提供的框架，国内访问加载速度极慢</p><p>YouTube</p><p>720yun：站在访问者的角度这个应该是最好的，但因为他是第三方商业所以我们并不推荐</p>',
         stackedButtons: true,
         cssClass: 'mdui-text-color-theme mdui-typo',
         buttons: [
             {
-                text: '自建源',
+                text: '自建源（Deprecated）',
                 onClick: function(list) {
-                    window.open('http://mc.jnyuyingmc.top:666/yymc/vrview/examples/yuying');
                 }
             },
             {
